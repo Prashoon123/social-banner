b11ed1f06fd31b8015ba673fea9561079106ff73
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2021-10-19 22:50:55 +0530

    test

--- pages/twitter-banner.js
+++ pages/twitter-banner.js
@@ -92,7 +92,6 @@ export async function getServerSideProps(context) {
   return {
     props: {
       image:
-        image ||
         "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format",
     },
   };
